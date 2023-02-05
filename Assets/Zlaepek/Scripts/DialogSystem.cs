600c8c70d809dc990c0845177bd3f10779f1a972
zlaepek <69634389+zlaepek@users.noreply.github.com>
2023-02-05 16:40:03 +0900

    대화 시스템 구현 (테스트 안 해봄)
    TIme line에 조건, csv파일 연동
    또는, interaction  스크립트와 연동

--- Assets/Zlaepek/Scripts/DialogSystem.cs
+++ Assets/Zlaepek/Scripts/DialogSystem.cs
@@ -6,11 +6,13 @@ using UnityEngine.UI;
 
 public class DialogSystem : MonoBehaviour
 {
+    /* UI Game Object */
     // 대사
     public TextMeshProUGUI dialogText;
     // 캐릭터 이름
     public TextMeshProUGUI characterName;
 
+    /* 대사 Class */
     [System.Serializable]
     public class Dialog
     {
@@ -18,15 +20,18 @@ public class DialogSystem : MonoBehaviour
         public string characterID;
 
         [Tooltip("대사 내용")]
-        public string[] contexts;
+        public string context;
 
-        [Tooltip("이벤트 번호")] // 대사 순서, 분기점 선택지 시 출력 관리
-        public string[] dialogEventID;
+        //[Tooltip("이벤트 번호")] // 대사 순서, 분기점 선택지 시 출력 관리
+        //public string[] dialogEventID;
 
-        [Tooltip("컷신 ID")] // 컷신 삽입용
-        public string[] cutSceneID;
+        //[Tooltip("컷신 ID")] // 컷신 삽입용
+        //public string[] cutSceneID;
     }
 
+    /* 파일 읽어오기
+    Input: 파일이름.csv (string)
+    Output: 대사 array (Dialog[]) */
     public Dialog[] ParseDialog(string _CSVFileName)
     {
         List<Dialog> dialogList = new List<Dialog>(); // 대사 리스트 생성
@@ -38,40 +43,11 @@ public class DialogSystem : MonoBehaviour
         {
             string[] row = data[i].Split(new char[] { ',' }); //, 단위로 row 줄에 저장
 
-            Dialog dialog = new Dialog(); // 대사 리스트 생성
-            dialog.characterID = row[1];
-            List<string> contextList = new List<string>(); // 대사 리스트 생성
-            List<string> eventList = new List<string>(); // 이벤트 넘버 생성
-            List<string> cutSceneList = new List<string>(); // 컷신 있을때 표기용
+            Dialog dialog = new Dialog(); // 이번 열에 해당하는 대사
+            dialog.characterID = row[0];
+            dialog.context = row[1];
 
-            do
-            {
-                contextList.Add(row[2]);
-                eventList.Add(row[3]);
-                cutSceneList.Add(row[4]);
-
-                if (++i < data.Length) // i가 미리 증가한 상태에서 비교해준다 dataLength보다 작다면
-                {
-                    row = data[i].Split(new char[] { ',' });
-                }
-                else
-                {
-                    break;
-                }
-
-            } while (row[0].ToString() == ""); // 최초 1회 조건 비교 없이 한 차례 실행시키고 조건문을 비교
-                                               // row 0번째 줄에는 ID가 들어가 있고 Tostring으로 빈 공간인지 비교해줌
-
-
-            dialog.contexts = contextList.ToArray();
-            dialog.dialogEventID = eventList.ToArray();
-            dialog.cutSceneID = cutSceneList.ToArray();
-
-            dialogList.Add(dialog);
-
-            GameObject obj = GameObject.Find("DialogManager");
-            // obj.GetComponent<interactionEvent>().lineY = dialgoueList.Count;
-
+            dialogList.Add(dialog); // 이번 열 추가
         }
 
         return dialogList.ToArray();
@@ -80,36 +56,26 @@ public class DialogSystem : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetMouseButtonDown(0))
-        {
-            PushText();
-        }
-    }
-
-    private void PushText()
-    {
         Dialog[] currentEventDialog = ParseDialog("Sample.csv");
+        StartCoroutine(StartDialogueCoroutine(currentEventDialog));
+    }
 
+    // ref) https://onecoke.tistory.com/entry/%EC%9C%A0%EB%8B%88%ED%8B%B02D-%EB%8C%80%ED%99%94-%EC%9D%B4%EB%B2%A4%ED%8A%B8-%EA%B5%AC%ED%98%84-%EC%97%B0%EC%8A%B5
+    /* Dialog 한 문장 씩 출력 */
+    IEnumerator StartDialogueCoroutine(Dialog[] currentEventDialog)
+    {
+        // 한문장 당
         for (int i = 0; i < currentEventDialog.Length; i++)
         {
-            dialogText.text = currentEventDialog[i].contexts[0];
+            // 캐릭터 //
+            characterName.text = currentEventDialog[i].characterID;
+            // 대사 //
+            for (int j = 0; j < currentEventDialog[i].context.Length; j++)
+            {
+                dialogText.text += currentEventDialog[i].context[j]; // 한글자 씩 출력
+                yield return new WaitForSeconds(0.01f);
+            }
+            yield return new WaitForSeconds(0.1f);
         }
-        /*
-        currentTextLine += 1;
-        if (currentTextLine >= currentDialogue.line.Count)
-        {
-            Conclude();
-        }
-        else
-        {
-            targetText.text = currentDialogue.line[currentTextLine];
-        }
-        */
     }
 }
