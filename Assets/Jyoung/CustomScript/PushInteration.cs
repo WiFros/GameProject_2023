c50e5468265ac7e2fba7a149b334f5eb962f918c
WiFros <39113029+WiFros@users.noreply.github.com>
2023-02-09 08:05:13 +0900

    상호작용3
    상호작용 응용가능하도록 코드 뜯어보는중

--- Assets/Jyoung/CustomScript/PushInteration.cs
+++ Assets/Jyoung/CustomScript/PushInteration.cs
@@ -1,18 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using AC;
 
 public class PushInteration : MonoBehaviour
 {
+    public Vector3 dic = new Vector3();
+    public Transform transForm;
+    public int direction;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        direction = AC.Kick
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        var player = collision.gameObject;
+        if (collision.gameObject.name == "Player")
+        {
+            dic = gameObject.transform.position - player.transform.position;
+        }
     }
 }
