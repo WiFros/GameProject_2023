b8638d706507f19fe8c48df539dd43136f502e4e
jiwon <heich4088@naver.com>
2023-11-05 14:58:24 +0900

    캐릭터 텍스트 적용 및 보스전 진행 중

--- Assets/Script/Boss/PatternController.cs
+++ Assets/Script/Boss/PatternController.cs
@@ -11,4 +11,14 @@ public class PatternController : MonoBehaviour
     {
         // 패턴 종료 로직
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            gameObject.tag = "Untagged";
+            Debug.Log("보스의 범위에 들어섰습니다.");
+            Debug.Log("보스가 도망갑니다.");
+        }
+    }
 }
