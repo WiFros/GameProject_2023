a05b7f00c0437ff1c8f2960b9566f6e4d438c839
jiwon <heich4088@naver.com>
2023-11-12 21:03:13 +0900

    보스전 구현중

--- Assets/Script/Boss/PatternController.cs
+++ Assets/Script/Boss/PatternController.cs
@@ -2,6 +2,21 @@ using UnityEngine;
 
 public class PatternController : MonoBehaviour
 {
+    Vector3[] spawn = new Vector3[12]; //배열의 크기는 햇불의 갯수, 스폰 되는 장소 저장
+    Vector3 targetposition;
+    TouchOnOff too;
+    public bool bossspawn = false;
+    public int target;
+
+    private void Start()
+    {
+        too = GameObject.Find("TouchManager").GetComponent<TouchOnOff>(); //토치 매니저에서 토치온오프 함수 가져오기
+    }
+
+    private void Update()
+    {
+        transform.position = Vector3.MoveTowards(gameObject.transform.position, targetposition, 10); //목표를 향해 이동)
+    }
     public void StartPattern()
     {
         // 패턴 시작 로직
@@ -16,9 +31,24 @@ public class PatternController : MonoBehaviour
     {
         if(other.gameObject.tag == "Player")
         {
+            bossspawn = false;
             gameObject.tag = "Untagged";
             Debug.Log("보스의 범위에 들어섰습니다.");
             Debug.Log("보스가 도망갑니다.");
+        }
+    }
+
+    public void SpawnBoss(GameObject touch)
+    {
+        for (int i = 0; i < too.touchlight.Length; i++)
+        {
+            if(too.touches[i] == touch)
+            {
+                bossspawn = true;
+                target = i;
+                int a = Random.Range(1, 3);
+                targetposition = spawn[a * i];
+            }
         }
     }
 }
--- Assets/Script/Interaction/TouchOnOff.cs
+++ Assets/Script/Interaction/TouchOnOff.cs
@@ -4,26 +4,48 @@ using UnityEngine;
 
 public class TouchOnOff : MonoBehaviour
 {
-    public bool touchlight = false;
+    public bool[] touchlight = new bool[6];
+    public GameObject[] touches = new GameObject[6];
+
+    PatternController bosspattern;
 
     void Start()
     {
-        
+        bosspattern = GameObject.Find("Boss").GetComponent<PatternController>(); //보스 오브젝트의 패턴 함수 가져오기
+
     }
 
     void Update()
     {
-        
+        if (bosspattern.bossspawn)
+        {
+            //touches[bosspattern.target] 점화 애니메이션 구현 부분
+        }
     }
 
     private void OnTriggerStay(Collider other)
     {
         if(other.gameObject.tag == "Player" && Input.GetKeyDown(KeyCode.F))
         {
-            touchlight = true;
+            for (int i = 0; i < touches.Length; i++)
+            {
+                if(touches[i] == gameObject)
+                {
+                    touchlight[i] = true;
+                    bosspattern.SpawnBoss(touches[i]);
+                    break;
+                }
+            }
         }else if(other.gameObject.tag == "Boss")
         {
-            touchlight = false;
+            for (int i = 0; i < touches.Length; i++)
+            {
+                if (touches[i] == gameObject)
+                {
+                    touchlight[i] = false;
+                    break;
+                }
+            }
         }
     }
 }
